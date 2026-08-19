e7e65ec1ceda95c68503cfcd470724b19c693e0e
Juliana <juliana.rocha.dl@gmail.com>
2026-08-19 10:33:30 -0300

    Criando a tabela guardians

--- schema.sql
+++ schema.sql
@@ -11,6 +11,22 @@ create table students(
     contact char(11),
     gradeLevel enum('4° ano EF', '5° ano EF', '6° ano EF', '7° ano EF', '8° ano EF', '9° ano EF', '1° ano EM', '2° ano EM', '3° ano EM')
 );
+
+create table guardians(
+	idGuardian int auto_increment primary key,
+    idStudent int,
+	Fname varchar(20),
+    Minit varchar(20),
+    Lname varchar(20),
+    CPF char(11),
+    contact char(11),
+    constraint fk_guardians_student foreign key (idStudent) references students(idStudent)
+);
+
+
+    
+
+
     
     
     
