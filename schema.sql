32ccd20364c7aac92ed885881f3f1c58362df4bd
Juliana <juliana.rocha.dl@gmail.com>
2026-08-20 02:21:40 -0300

    Editando o diagrama e adicionando algumas tabelas

--- schema.sql
+++ schema.sql
@@ -1,53 +1,99 @@
 create database tutoring_agency;
 
+use tutoring_agency;
+
 create table students(
 	idStudent int auto_increment primary key,
-    Fname varchar(20),
+    idGuardian int not null,
+    idNeighborhood int not null,
+    street varchar(20) not null,
+	num varchar(6) not null,
+    Fname varchar(20) not null,
     Minit varchar(20),
-    Lname varchar(20),
-    CPF char(11),
-    streetNumber varchar(20),
-    neighborhoodCityState varchar(20),
+    Lname varchar(20) not null,
+    CPF char(11) not null unique,
     contact char(11),
-    gradeLevel enum('4° ano EF', '5° ano EF', '6° ano EF', '7° ano EF', '8° ano EF', '9° ano EF', '1° ano EM', '2° ano EM', '3° ano EM')
+    gradeLevel enum('4° ano EF', '5° ano EF', '6° ano EF', '7° ano EF', '8° ano EF', '9° ano EF', '1° ano EM', '2° ano EM', '3° ano EM') not null,
+    constraint fk_students_guardian foreign key (idGuardian) references guardians(idGuardian),
+    constraint fk_students_neighborhood foreign key (idNeighborhood) references neighborhoods(idNeighborhood)
 );
 
 create table guardians(
 	idGuardian int auto_increment primary key,
-    idStudent int,
-	Fname varchar(20),
+	Fname varchar(20) not null,
     Minit varchar(20),
-    Lname varchar(20),
-    CPF char(11),
-    contact char(11),
-    constraint fk_guardians_student foreign key (idStudent) references students(idStudent)
+    Lname varchar(20) not null,
+    CPF char(11) not null unique,
+    contact char(11) not null
 );
 
 create table subjects(
 	idSubject int auto_increment primary key,
-    Dname varchar(20)
+    Dname varchar(20) not null
 );
 
 create table teachers(
 	idTeacher int auto_increment primary key,
-	Fname varchar(20),
+    idNeighborhood int not null,
+    street varchar(20) not null,
+	num varchar(6) not null,
+	Fname varchar(20) not null,
     Minit varchar(20),
-    Lname varchar(20),
-    CNPJ char(14),
-    streetNumber varchar(20),
-    neighborhoodCityState varchar(20),
-    contact char(11)
+    Lname varchar(20) not null,
+    CNPJ char(14) not null unique,
+    CPF char(11) unique,
+    contact char(11) not null,
+	constraint fk_teachers_neighborhood foreign key (idNeighborhood) references neighborhoods(idNeighborhood)
 );
 
 create table teacherSubject(
 	idTeacher int,
     idSubject int,
-    gradeLevel enum('4° ano EF', '5° ano EF', '6° ano EF', '7° ano EF', '8° ano EF', '9° ano EF', '1° ano EM', '2° ano EM', '3° ano EM'),
+    gradeLevel enum('4° ano EF', '5° ano EF', '6° ano EF', '7° ano EF', '8° ano EF', '9° ano EF', '1° ano EM', '2° ano EM', '3° ano EM') not null,
     primary key (idTeacher, idSubject),
     constraint fk_TS_teacher foreign key (idTeacher) references teachers(idTeacher),
     constraint fk_TS_subject foreign key (idSubject) references subjects(idSubject)
 );
 
+create table classes(
+	idClasse int auto_increment primary key,
+    idTeacher int not null,
+    idStudent int not null,
+    idSubject int not null,
+    idRecurringClasse int,
+    classDate date not null,
+    classTime time not null,
+    typeClasse enum('Fixa', 'Esporádica'),
+    classStatus enum('Agendada', 'Reagendada', 'Realizada', 'Cancelada'),
+    constraint fk_classes_teacher foreign key (idTeacher) references teachers(idTeacher),
+    constraint fk_classes_student foreign key (idStudent) references students(idStudent),
+	constraint fk_classes_subject foreign key (idSubject) references subjects(idSubject),
+    constraint fk_classes_recurringClasse foreign key (idRecurringClasse) references recurringClasses(idRecurringClasse)
+);
+
+create table recurringClasses(
+	idRecurringClasse int auto_increment primary key,
+    dayWeek enum('segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado', 'domingo') not null,
+    timeClasse time not null,
+    startDate date not null,
+    endDate date
+);
+    
+create table neighborhoods(
+	idNeighborhood int auto_increment primary key,
+    Bnome varchar(20) not null,
+    city varchar(20) not null,
+    state char(2) not null,
+    cityZone enum('Norte', 'Sul', 'Leste', 'Oeste') not null
+);
+
+    
+
+    
+    
+    
+    
+    
 
     
 	
