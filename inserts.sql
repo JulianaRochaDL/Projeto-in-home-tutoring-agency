9c6ecc2319312369caaf901c748b186ae4b43232
Juliana <juliana.rocha.dl@gmail.com>
2026-08-20 11:32:18 -0300

    inserindo dados na tabela neighborhoods

---
+++ inserts.sql
@@ -0,0 +1,37 @@
+-- Inserindo dados nas tabelas
+
+use tutoring_agency;
+
+insert into neighborhoods (Bnome, city, state, cityZone)
+	values 
+		('Centro', 'Caruaru', 'PE', 'Norte'),
+        ('Boa Vista', 'Caruaru', 'PE', 'Norte'),
+        ('Indianópolis', 'Caruaru', 'PE', 'Sul'),
+        ('Maurício de Nassau', 'Caruaru', 'PE', 'Leste'),
+        ('Petrópolis', 'Caruaru', 'PE', 'Oeste'),
+        ('Kennedy', 'Caruaru', 'PE', 'Sul'),
+        ('Salgado', 'Caruaru', 'PE', 'Sul'),
+        ('Alto do Moura', 'Caruaru', 'PE', 'Oeste'),
+        ('Cidade Alta', 'Caruaru', 'PE', 'Norte'),
+        ('Universitário', 'Caruaru', 'PE', 'Sul'),
+        ('Boa Viagem', 'Recife', 'PE', 'Sul'),
+        ('Setúbal', 'Recife', 'PE', 'Sul'),
+        ('Imbiribeira', 'Recife', 'PE', 'Sul'),
+        ('Casa Forte', 'Recife', 'PE', 'Norte'),
+        ('Casa Amarela', 'Recife', 'PE', 'Norte'),
+        ('Madalena', 'Recife', 'PE', 'Oeste'),
+        ('Iputinga', 'Recife', 'PE', 'Oeste'),
+        ('Várzea', 'Recife', 'PE', 'Oeste'),
+        ('Boa Vista', 'Recife', 'PE', 'Norte'),
+        ('Espinheiro', 'Recife', 'PE', 'Norte'),
+        ('Barra', 'Salvador', 'BA', 'Sul'),
+		('Pituba', 'Salvador', 'BA', 'Leste'),
+		('Itapuã', 'Salvador', 'BA', 'Leste'),
+		('Brotas', 'Salvador', 'BA', 'Oeste'),
+		('Rio Vermelho', 'Salvador', 'BA', 'Leste'),
+		('Federação', 'Salvador', 'BA', 'Oeste'),
+		('Cabula', 'Salvador', 'BA', 'Norte'),
+		('Liberdade', 'Salvador', 'BA', 'Norte'),
+		('Ondina', 'Salvador', 'BA', 'Sul'),
+		('Pernambués', 'Salvador', 'BA', 'Norte');
+		
